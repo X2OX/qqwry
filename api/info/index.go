16aac24d62f316e36c5ba1c8ca581e469a85b286
Aoang <aoang@x2ox.com>
2022-01-10 12:38:17 +0800

    Create index.go

---
+++ api/info/index.go
@@ -0,0 +1,64 @@
+package qqwry
+
+import (
+	"encoding/json"
+	"net"
+	"net/http"
+	"strings"
+
+	"go.x2ox.com/utils/cors"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	cors.CORS(w, r, Info)
+}
+
+func NewRequestInfo(r *http.Request) *RequestInfo {
+	if r == nil {
+		return nil
+	}
+	b, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	return &RequestInfo{
+		Method:           r.Method,
+		URL:              r.URL,
+		Proto:            r.Proto,
+		ProtoMajor:       r.ProtoMajor,
+		ProtoMinor:       r.ProtoMinor,
+		Header:           r.Header,
+		ContentLength:    r.ContentLength,
+		TransferEncoding: r.TransferEncoding,
+		Close:            r.Close,
+		Host:             r.Host,
+		Form:             r.Form,
+		PostForm:         r.PostForm,
+		MultipartForm:    r.MultipartForm,
+		Trailer:          r.Trailer,
+		RemoteAddr:       r.RemoteAddr,
+		RequestURI:       r.RequestURI,
+		TLS:              r.TLS,
+		Body:             string(b),
+	}
+}
+
+type RequestInfo struct {
+	Method           string
+	URL              *url.URL
+	Proto            string // "HTTP/1.0"
+	ProtoMajor       int    // 1
+	ProtoMinor       int    // 0
+	Header           http.Header
+	ContentLength    int64
+	TransferEncoding []string
+	Close            bool
+	Host             string
+	Form             url.Values
+	PostForm         url.Values
+	MultipartForm    *multipart.Form
+	Trailer          http.Header
+	RemoteAddr       string
+	RequestURI       string
+	TLS              *tls.ConnectionState
+	Body             string
+}
